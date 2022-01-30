46155e62107f2376b6f24a27659db64bf2c6d047
fatmakoyuncu <fatmakyc16@gmail.com>
2022-01-30 22:43:33 +0300

    Initial Commit

---
+++ IAmRichApp/IAmRichAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  IAmRichAppApp.swift
+//  IAmRichApp
+//
+//  Created by Senol Pazar on 30.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct IAmRichAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
